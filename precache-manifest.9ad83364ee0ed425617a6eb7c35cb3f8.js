79610a1319f1b3c1fe2d9587085044ac80aa2e54
Keith Beaudoin <bowdwin.muninted@gmail.com>
2020-08-10 16:14:45 -0400

    Updates

---
+++ precache-manifest.9ad83364ee0ed425617a6eb7c35cb3f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bdf186592730858f4446de93795918f",
+    "url": "/react-employee-tracker/index.html"
+  },
+  {
+    "revision": "f932d2cea16f970b38da",
+    "url": "/react-employee-tracker/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "e75dc7dee6ed39b44ea9",
+    "url": "/react-employee-tracker/static/css/main.1818356c.chunk.css"
+  },
+  {
+    "revision": "f932d2cea16f970b38da",
+    "url": "/react-employee-tracker/static/js/2.304c203c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-employee-tracker/static/js/2.304c203c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e75dc7dee6ed39b44ea9",
+    "url": "/react-employee-tracker/static/js/main.500496df.chunk.js"
+  },
+  {
+    "revision": "76dc261f1df6dbdf0089",
+    "url": "/react-employee-tracker/static/js/runtime-main.74e74e4e.js"
+  }
+]);
